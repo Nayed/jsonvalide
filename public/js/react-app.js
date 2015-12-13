12486de0a2319874d4b19d3aeb6ed50e45dd7cf6
Nayed SAID ALI <nayed.saidali@gmail.com>
2015-12-14 00:02:53 +0100

    json valide

--- public/js/react-app.js
+++ public/js/react-app.js
@@ -19655,6 +19655,8 @@
 
 	'use strict';
 
+	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
 	Object.defineProperty(exports, "__esModule", {
 	    value: true
 	});
@@ -19665,12 +19667,121 @@
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	var ReactApp = function ReactApp(props) {
-	    return _react2.default.createElement(
-	        'div',
-	        null,
-	        'This is a React app!'
-	    );
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var ReactApp = (function (_Component) {
+	    _inherits(ReactApp, _Component);
+
+	    function ReactApp(props) {
+	        _classCallCheck(this, ReactApp);
+
+	        var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(ReactApp).call(this, props));
+
+	        var placeHolder = {
+	            try: 'removing some whitespace',
+	            or: 'add valid JSON',
+	            then: ['then', 'click', 'format'],
+	            hi: 'mom!!!'
+	        };
+
+	        _this.state = {
+	            value: JSON.stringify(placeHolder, null, 4),
+	            txtClass: styles.textArea
+	        };
+	        return _this;
+	    }
+
+	    _createClass(ReactApp, [{
+	        key: '_validate',
+	        value: function _validate() {
+	            try {
+	                var orig = JSON.parse(this.state.value);
+	                var v = JSON.stringify(orig, null, 4);
+	                this.setState({
+	                    value: v,
+	                    error: null,
+	                    txtClass: styles.textArea
+	                });
+	            } catch (e) {
+	                this.setState({
+	                    error: 'Invalid JSON: ' + e.name + ' - ' + e.message,
+	                    txtClass: styles.textAreaError
+	                });
+	            }
+	        }
+	    }, {
+	        key: '_saveText',
+	        value: function _saveText(e) {
+	            this.setState({ value: e.target.value });
+	        }
+	    }, {
+	        key: 'render',
+	        value: function render() {
+	            return _react2.default.createElement(
+	                'div',
+	                { id: 'app', style: styles.container },
+	                _react2.default.createElement(
+	                    'h1',
+	                    { style: styles.header },
+	                    'Format & Validate JSON'
+	                ),
+	                _react2.default.createElement(
+	                    'p',
+	                    { style: styles.error },
+	                    this.state.error
+	                ),
+	                _react2.default.createElement('textArea', {
+	                    rows: '30',
+	                    cols: '80',
+	                    value: this.state.value,
+	                    style: this.state.txtClass,
+	                    onChange: this._saveText.bind(this)
+	                }),
+	                _react2.default.createElement(
+	                    'button',
+	                    { style: styles.button, onClick: this._validate.bind(this) },
+	                    'VALIDATE'
+	                )
+	            );
+	        }
+	    }]);
+
+	    return ReactApp;
+	})(_react.Component);
+
+	var styles = {
+	    container: {
+	        display: 'flex',
+	        flexDirection: 'column',
+	        alignItems: 'center',
+	        fontFamily: "'Helvetica Neue', Helvetica, Arial, sans-serif"
+	    },
+	    header: {
+	        flex: '0 1 auto'
+	    },
+	    error: {
+	        color: 'red'
+	    },
+	    textArea: {
+	        flex: '0 1 auto',
+	        fontFamily: 'monospace'
+	    },
+	    textAreaError: {
+	        flex: '0 1 auto',
+	        fontFamily: 'monospace',
+	        border: '1px solid red'
+	    },
+	    button: {
+	        cursor: 'pointer',
+	        flex: '0 1 auto',
+	        marginTop: 20,
+	        padding: 10,
+	        fontSize: 14
+	    }
 	};
 
 	exports.default = ReactApp;
--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 'use strict'
 
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import ReactApp from './es5-lib/react-app.js'
+
 import express from 'express'
 const app = express()
 
 app.use('/static', express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    const rApp = React.createFactory(ReactApp)({})
+    const reactHtml = ReactDOMServer.renderToString(rApp)
+
+    const html = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>JSON Formatter</title>
+      </head>
+      <body>
+        <div id="app">${reactHtml}</div>
+        <script src="/static/js/react-app.js"></script>
+      </body>
+    </html>
+    `
+    res.send(html)
 })
 
 const server = app.listen(9000, () => {
